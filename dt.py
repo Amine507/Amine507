f80b2c638de1019edc0243e0ebac9e0ab1fec4fe
wazofake <jerome.adriaens@gmail.com>
2021-10-07 21:49:06 +0200

    Plot works ?

--- dt.py
+++ dt.py
@@ -8,7 +8,6 @@ Project 1 - Classification algorithms
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from plot import plot_boundary
 from data import make_balanced_dataset, make_unbalanced_dataset
 from sklearn.tree import DecisionTreeClassifier
@@ -18,26 +17,28 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 if __name__ == "__main__":
 
     n_points = 3000  # We want 3000 samples
-    min_samples = [2, 8, 32, 64, 128, 500]
-    acc_scores = np.zeros(len(min_samples))
-    X, y = make_unbalanced_dataset(n_points, 50)
+    min_samples_split = [2, 8, 32, 64, 128, 500]
+    acc_scores = np.zeros((len(min_samples_split)))
+    
+    
     #crée 3000 points avec une valeur associée à chaque point
     #X matrice 3000x2 avec les coord de chaque point
     #y liste 3000 valeurs correspondant à chaque point
+    X, y = make_unbalanced_dataset(n_points, 50)
 
-    X_l = X[:1000]
-    X_t = X[1000:]
-    y_l = y[:1000]
-    y_t = y[1000:]
-    
+  
     # 1000 samples for the training, 2000 for the test
     #X_l matrice 1000x2 des points qui ont servi à l'entrainement
     #X_t matrice 1000x2 des points qui ont servi au test
     #y_l liste 1000 des valeurs de points d'entrainement
     #y_t liste 2000 des valeurs de points de test
+    X_l = X[:1000]
+    X_t = X[1000:]
+    y_l = y[:1000]
+    y_t = y[1000:]
 
-    for i, mss in enumerate(min_samples):
+    for i, mss in enumerate(min_samples_split):
         DecisionTree = DecisionTreeClassifier(min_samples_split=mss).fit(X_l, y_l) #create the tree
         y_p = DecisionTree.predict(X_t) #test the tree
         acc_scores[i] = accuracy_score(y_t, y_p)  # table of scores
-        plot_boundary('dt_min_samples={}'.format(value), estimator,X_t, y_t, mesh_step_size=0.1, title="")
+        plot_boundary('dt_min_samples'+str(i), DecisionTree,X_t, y_t, mesh_step_size=0.1, title="Decision Tree with min_samples_split="+str(mss))
